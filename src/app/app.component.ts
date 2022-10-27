e33b4e7fcf907caa8224a31832ff8054c6bcc50b
MinhHoang3011 <minhhoang301198@gmail.com>
2022-10-27 11:23:51 +0700

    done

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {Tour} from "./model/tour";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FinalTest';
+}
